c64294c065ab3c0e87f995c5550448be273873fa
fridokus <raxomukus@gmail.com>
2023-02-16 10:16:37 +0100

    update bot with correct command definitions 🧦

--- bot.py
+++ bot.py
@@ -4,29 +4,28 @@ from discord.ext import commands
 
 TOKEN = open('.token.txt').read().split()[0]
 intents = discord.Intents.all()
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-bot = commands.Bot(command_prefix='!')
-
-@bot.command()
+@bot.command(name='rank')
 async def rank(ctx, arg):
     await ctx.send(arg)
 
-@bot.command()
+@bot.command(name='add')
 async def add(ctx, a: int, b: int):
     await ctx.send(a + b)
 
-@client.event
-async def on_ready(): print(f'{client.user} has connected to Discord!')
+@bot.event
+async def on_ready(): print(f'{bot.user} has connected to Discord!')
 
-@client.event
+@bot.event
 async def on_message(message):
+    await bot.process_commands(message)
     u = str(message.author).split("#")[0]
     m = str(message.content)
 
     print(f'Message {m} by {u}')
 
-    if message.author == client.user: return
+    if message.author == bot.user: return
 
     if m.lower() in ["tjena", "hej"]:
         await message.channel.send(f'Tjenixen, {u}')
@@ -37,4 +36,4 @@ async def on_message(message):
     if 'pontus' in m.lower():
         await message.add_reaction('🔢')
 
-client.run(TOKEN)
+bot.run(TOKEN)

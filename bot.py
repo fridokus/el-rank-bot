2ac48ec2cebb61571e8ed4464a08ae3617c6a9f4
fridokus <raxomukus@gmail.com>
2023-02-17 13:45:10 +0100

    Work on bot 👑

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import discord
 from discord.ext import commands
 import json
 import requests
+import re
 
 headers = {
     'authority': 'gql-gateway-dot-slippi.uc.r.appspot.com',
@@ -45,49 +46,51 @@ TOKEN = open('.token.txt').read().split()[0]
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+def parse_slippi_user(arg):
+    if 'pontus'  == arg.lower(): arg = 'THN#131'
+    elif 'deg'   == arg.lower(): arg = 'pa240'
+    elif 'zoler' == arg.lower(): arg = 'pa240'
+    m = re.search(r'([a-zA-Z]{2,}).?(\d{2,})', arg)
+    return ''.join((m.group(1).upper(), '#', m.group(2))) if m else None
+
 @bot.command(name='rating')
 async def rating(ctx, arg):
-    if 'pontus' == arg.lower(): arg = 'THN#131'
-    elif 'deg' == arg.lower(): arg = ''
-    elif 'zoler' == arg.lower(): arg = ''
-    if '-' in arg: user = arg.replace('-', '#')
-    elif '#' in arg: user = arg
-    else:
+    user = parse_slippi_user(arg)
+    if not user:
         ctx.send('Get the rating of a user using this format: PA#240 . %s is invalid' % arg)
         return
-    user = user.upper()
     d = get_slippi_user(user)
     if not d: 
         ctx.send('User %s not found' % user)
         return
     username = d['data']['getConnectCode']['user']['displayName']
     rating = d['data']['getConnectCode']['user']['rankedNetplayProfile']['ratingOrdinal']
-    ret = username + ' (' + user + ') has ' + str(rating) + ' rating. '
-    if   rating >= 2800: ret += 'This guy/girl might be the G.O.A.T 🐐'
-    elif rating >= 2600: ret += 'This guy is a god ⛪️'
-    elif rating >= 2400: ret += 'Better than the best GnW in the world 🏴'
-    elif rating >= 2300: ret += 'Shoot for the moon 🌚'
-    elif rating >= 2200: ret += 'Don\'t wanna run into this BEAST 🦁'
-    elif rating >= 2150: ret += 'This is higher than Pontus will ever get 📈'
-    elif rating >= 2100: ret += 'Whoa Daddy! 🧔'
-    elif rating >= 2050: ret += 'I guess you play Fox 🦊'
-    elif rating >= 2000: ret += 'Diamond BOYS 💎'
-    elif rating >= 1900: ret += 'Respectable 🙏'
-    elif rating >= 1800: ret += 'Pump the numbers 💹'
-    elif rating >= 1700: ret += 'Not bad 🐘'
-    elif rating >= 1600: ret += 'Give this guy/girl a gold medal 🥇'
-    elif rating >= 1500: ret += 'Git gud 🐅'
-    elif rating >= 1400: ret += 'Need more practice against Falco 🐦️'
-    elif rating >= 1350: ret += 'Great things start small 🪦'
-    elif rating >= 1300: ret += 'You are a hard worker and a beautiful human being 👶'
-    elif rating >= 1200: ret += 'Do you know how to wavedash? 🌊'
-    elif rating >= 1100: ret += 'Do you know how to CC? 💀'
-    elif rating >= 1000: ret += 'Do you know how to L-cancel? 🍂'
-    elif rating >=  900: ret += 'Do you know how to short-hop? 🐇'
-    elif rating >=  800: ret += 'Do you know how to fast-fall? 🌠'
-    elif rating >=  780: ret += 'Super Smash Brothers Melee for the Nintendo Gamecube 🧊'
-    elif rating >=  700: ret += 'Your rating is about as low as the lowest I\'ve seen 🍃 But don\'t give up!'
-    else               : ret += 'You must have a girl-/boyfriend and I hope you have found meaning in life'
+    if   rating >= 2800: comment = 'This guy/girl might be the G.O.A.T 🐐'
+    elif rating >= 2600: comment = 'This guy is a god ⛪️'
+    elif rating >= 2400: comment = 'Better than the best GnW in the world 🏴'
+    elif rating >= 2300: comment = 'Shoot for the moon 🌚'
+    elif rating >= 2200: comment = 'Don\'t wanna run into this BEAST 🦁'
+    elif rating >= 2150: comment = 'This is higher than Pontus will ever get 📈'
+    elif rating >= 2100: comment = 'Whoa Daddy! 🧔'
+    elif rating >= 2050: comment = 'I guess you play Fox 🦊'
+    elif rating >= 2000: comment = 'Diamond BOYS 💎'
+    elif rating >= 1900: comment = 'Respectable 🙏'
+    elif rating >= 1800: comment = 'Pump the numbers 💹'
+    elif rating >= 1700: comment = 'Not bad 🐘'
+    elif rating >= 1600: comment = 'Give this guy/girl a gold medal 🥇'
+    elif rating >= 1500: comment = 'Git gud 🐅'
+    elif rating >= 1400: comment = 'Need more practice against Falco 🐦️'
+    elif rating >= 1350: comment = 'Great things start small 🪦'
+    elif rating >= 1300: comment = 'You are a hard worker and a beautiful human being 👶'
+    elif rating >= 1200: comment = 'Do you know how to wavedash? 🌊'
+    elif rating >= 1100: comment = 'Do you know how to CC? 💀'
+    elif rating >= 1000: comment = 'Do you know how to L-cancel? 🍂'
+    elif rating >=  900: comment = 'Do you know how to short-hop? 🐇'
+    elif rating >=  800: comment = 'Do you know how to fast-fall? 🌠'
+    elif rating >=  780: comment = 'Super Smash Brothers Melee for the Nintendo Gamecube 🧊'
+    elif rating >=  700: comment = 'Your rating is about as low as the lowest I\'ve seen 🍃 But don\'t give up!'
+    else               : comment = 'You must have a girl-/boyfriend and I hope you have found meaning in life'
+    ret = f'{username} ({user}) has {rating} rating. {comment}'
     await ctx.send(ret)
 
 @bot.command(name='add')
@@ -102,18 +105,20 @@ async def on_message(message):
     await bot.process_commands(message)
     u = str(message.author).split("#")[0]
     m = str(message.content)
-
     print(f'Message {m} by {u}')
-
     if message.author == bot.user: return
 
-    if m.lower() in ["tjena", "hej"]:
-        await message.channel.send(f'Tjenixen, {u}')
-    elif m.lower() == "bye":
-        await message.channel.send(f'Bye {u}')
-    if 'zoler' in m.lower():
+    if m.lower() in ("tjena", "hej"):      await message.channel.send(f'Tjenixen, {u}')
+    elif m.lower() in ("bye", 'hejsvejs'): await message.channel.send(f'Bye {u}')
+    elif m.lower() in ('gn', 'godnatt'):   await message.channel.send(f'GN, {u}')
+    elif m.lower() in ('gm', 'godmorgon', 'god morgon'):
+        await message.channel.send(f'GN, {u}')
+
+    if all(i in m.lower() for i in ('zoler', 'fox')):
         await message.add_reaction('🦊')
-    if 'pontus' in m.lower():
+    if all(i in m.lower() for i in ('pontus', 'fuck')):
         await message.add_reaction('🔢')
+    if all(i in m.lower() for i in ('rax', 'king')):
+        await message.add_reaction('👑')
 
 bot.run(TOKEN)
